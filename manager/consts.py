81223a94acf9f0c501c3597fe9db344ef8e3b007
AEpolyakov <ae.polyakov@mail.ru>
2021-05-27 10:23:04 +0400

    types refactored

--- manager/consts.py
+++ manager/consts.py
@@ -1,9 +1,25 @@
-TYPE_CHOICES = (('0', 'админ.'),
-                ('1', 'отгул'),
-                ('2', 'больн.'),
-                ('3', 'отпуск'),
-                ('4', 'команд.'),
+TYPE_NONE = '-----'
+TYPE_ADMIN = 'админ.'
+TYPE_OTGUL = 'отгул'
+TYPE_HOSP = 'больн.'
+TYPE_OTPUSK = 'отпуск'
+TYPE_KOMAND = 'команд.'
+
+ID_NONE = '0'
+ID_ADMIN = '1'
+ID_OTGUL = '2'
+ID_HOSP = '3'
+ID_OTPUSK = '4'
+ID_KOMAND = '5'
+
+TYPE_CHOICES = ((ID_NONE, TYPE_NONE),
+                (ID_ADMIN, TYPE_ADMIN),
+                (ID_OTGUL, TYPE_OTGUL),
+                (ID_HOSP, TYPE_HOSP),
+                (ID_OTPUSK, TYPE_OTPUSK),
+                (ID_KOMAND, TYPE_KOMAND),
                 )
+
 
 STR_MONTH = ('', 'январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сенябрь',
              'октябрь', 'ноябрь', 'декабрь')
