40098ffa97a8f0c1cc044b30759ef0f9429fce93
AEpolyakov <ae.polyakov@mail.ru>
2021-05-31 16:10:18 +0400

    autodate input added

--- manager/static/manager/timesheet.js
+++ manager/static/manager/timesheet.js
@@ -1,10 +1,19 @@
 window.onload = function() {
-    const timesheet_date = document.getElementById('id_date')
-    timesheet_date.value = today()
+    const timesheet_date = document.getElementById('timesheet-month')    
+    const form_date = document.querySelector('#id_date')
+    timesheet_date.value = today()  
+    form_date.value = timesheet_date.value
+    console.log(form_date)
 
     function today(){
-        const date = new Date()
-        const currentDate = date.toISOString().substring(0,10)
-        return currentDate
+        const date = new Date()        
+        const month = date.getMonth() + 1
+        const year = date.getFullYear()
+        console.log(year.toString() + '-' + ('0' + month.toString()).slice(-2))
+        return year.toString() + '-' + ('0' + month.toString()).slice(-2)
     }
+    timesheet_date.addEventListener('change', (event)=>{
+    	console.log(timesheet_date.value)
+    	form_date.value = timesheet_date.value
+    })
 }
